a4b69a703b9d65338896d9d0402cef1e46f92f15
Ville Peurala <ville.peurala@gmail.com>
2017-12-22 13:54:45 +0200

    Remove created temporary directory in the end.

--- shuffle-playlist.sh
+++ shuffle-playlist.sh
@@ -9,9 +9,12 @@ if [ -z $1 ]; then
   exit 2;
 else
   EXPORTED_PLAYLIST_FILE_NAME=$1;
-  echo "EXPORTED_PLAYLIST_FILE_NAME: ${EXPORTED_PLAYLIST_FILE_NAME}";
   TEMP_DIR=$(mktemp -d);
   cp ${EXPORTED_PLAYLIST_FILE_NAME} ${TEMP_DIR};
-  mac2unix --assume-utf16le --remove-bom -n ${TEMP_DIR}/${EXPORTED_PLAYLIST_FILE_NAME} ${TEMP_DIR}/phase1.txt;
+  mac2unix --assume-utf16le --remove-bom -n ${TEMP_DIR}/${EXPORTED_PLAYLIST_FILE_NAME} ${TEMP_DIR}/unix.txt;
+  head -n 1 ${TEMP_DIR}/unix.txt > ${TEMP_DIR}/header.txt;
+  tail -n +2 ${TEMP_DIR}/unix.txt | sort -R > ${TEMP_DIR}/songs.txt;
+  cat ${TEMP_DIR}/header.txt ${TEMP_DIR}/songs.txt;
+  rm -rf ${TEMP_DIR};
 fi
 
