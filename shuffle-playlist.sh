a13e7026c56e3eedc94ebb18ad0e0d706d94cfe7
Ville Peurala <ville.peurala@gmail.com>
2017-12-22 13:42:20 +0200

    Use mac2unix.

--- shuffle-playlist.sh
+++ shuffle-playlist.sh
@@ -10,5 +10,8 @@ if [ -z $1 ]; then
 else
   EXPORTED_PLAYLIST_FILE_NAME=$1;
   echo "EXPORTED_PLAYLIST_FILE_NAME: ${EXPORTED_PLAYLIST_FILE_NAME}";
+  TEMP_DIR=$(mktemp -d);
+  cp ${EXPORTED_PLAYLIST_FILE_NAME} ${TEMP_DIR};
+  mac2unix --assume-utf16le --remove-bom -n ${TEMP_DIR}/${EXPORTED_PLAYLIST_FILE_NAME} ${TEMP_DIR}/phase1.txt;
 fi
 
